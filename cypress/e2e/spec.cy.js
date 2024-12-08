4d59eb8565d389fc216124b6cddf4ea75b25965c
Marcos Daniel <marcosnumber11@gmail.com>
2024-12-08 18:52:10 -0300

    Testes E2E - Botão para limpar todas as tarefas completas e Botão para marcar todas as tarefas como completas

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -68,4 +68,51 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+
+  it('Marca todas as tarefas como completas', () => {
+    cy.visit(''); 
+
+    // Inserindo tarefas
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+
+    // Marcando todas as tarefas como completas
+    cy.get('[data-cy=toggle-all]')
+      .click();
+
+    // Verifica se todas as tarefas estão marcadas como completas
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .each(($el) => {
+        cy.wrap($el).should('be.checked');
+      });
+  });
+
+
+  it('Limpa tarefa completas', () => {
+    cy.visit(''); 
+
+    // Inserindo tarefas
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+
+    // Marcando todas as tarefas como completas
+    cy.get('[data-cy=toggle-all]')
+      .click();
+
+    // Insere mais uma tarefa
+    cy.get('[data-cy=todo-input]')
+      .type('Outra tarefa{enter}');
+
+    // Limpando tarefas completas
+    cy.get('[data-cy=clear-completed]')
+      .click();
+
+    // Verifica se apenas a tarefa adicionada por ultimo ainda existe
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1);
+
+  });
 });
